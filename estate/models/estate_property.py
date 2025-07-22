7f8cf47809488e3e1d13e70ced38882476f43762
fena-odoo <fena@odoo.com>
2025-07-22 16:24:10 +0700

    chapter 8 valid date, onchange garden tick

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -43,6 +43,15 @@ class EstateProperty(models.Model):
         ]
     )
     
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+    
     property_type_id = fields.Many2one(
         "estate.property.type",
         string="Property Type"
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from datetime import timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -11,3 +12,21 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+    
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(
+        compute='_compute_date_deadline',
+        inverse='_inverse_date_deadline',
+        store=True,
+    )
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date or fields.Date.today()
+            offer.date_deadline = create_date + timedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date or fields.Date.today()
+            offer.validity = (offer.date_deadline - create_date).days
