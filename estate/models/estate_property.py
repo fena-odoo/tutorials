fa69ff709b6ee173ff171f0d962348c48c61fff8
fena-odoo <fena@odoo.com>
2025-07-22 11:18:56 +0700

    chapter 5

---
+++ estate/models/estate_property.py
@@ -0,0 +1,44 @@
+from odoo import models, fields
+from datetime import date, timedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    active = fields.Boolean(default=True)  # reserved: controls visibility
+    state = fields.Selection(              # reserved: controls lifecycle stages
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        default='new',
+        required=True,
+        copy=False
+    )
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        string="Available From",
+        copy=False,
+        default=lambda self: date.today() + timedelta(days=90),
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
