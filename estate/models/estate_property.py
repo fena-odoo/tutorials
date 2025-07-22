319ac97a7f97ba224329d92a0c3b7df38f5f7998
fena-odoo <fena@odoo.com>
2025-07-22 15:29:56 +0700

    chapter 7 add offers

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -62,3 +62,9 @@ class EstateProperty(models.Model):
     )
 
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
+    
+    offer_ids = fields.One2many(
+        'estate.property.offer',
+        'property_id',
+        string="Offers"
+    )
