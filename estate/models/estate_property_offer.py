0d8e2cadb9c3f173593740fedc47800f78926332
fena-odoo <fena@odoo.com>
2025-07-23 11:41:54 +0700

    chapter 11 before search

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -15,7 +15,9 @@ class EstatePropertyOffer(models.Model):
     
     status = fields.Selection(
         [('accepted', 'Accepted'), ('refused', 'Refused')],
-        copy=False
+        copy=False,
+        store=True,
+        readonly=True
     )
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
