e22961e34d8996470fcddc551ba9ea951a1a7eab
fena-odoo <fena@odoo.com>
2025-07-23 10:37:47 +0700

    chapter 11 before form

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,8 +3,10 @@ from odoo import models, fields
 class EstatePropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Property Tag'
+    _order = "name"
 
     name = fields.Char(string='Name', required=True)
+    color = fields.Integer('Color Index')
     
     _sql_constraints = [
         ('unique_property_tag_name', 'UNIQUE(name)', 'The tag name must be unique.')
--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -3,9 +3,18 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property Type"
+    _order = "sequence, name"
 
     name = fields.Char(required=True)
+    sequence = fields.Integer('Sequence', default=1)
     
     _sql_constraints = [
         ('unique_property_type_name', 'UNIQUE(name)', 'The property type name must be unique.')
     ]
+    
+    property_ids = fields.One2many(
+        "estate.property", 
+        "property_type_id", 
+        string="Properties"
+    )
+
