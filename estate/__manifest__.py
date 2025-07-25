f913ea6333a25c924e3091367cda07b22e5b85a1
fena-odoo <fena@odoo.com>
2025-07-25 10:18:32 +0700

    add demo data add offer until ref

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,12 +4,15 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_menus.xml',
+        "data/master_property_types.xml",
         'views/estate_property_views.xml',
         'views/estate_property_offer_views.xml',
         'views/property_type_views.xml',
         'views/estate_property_tag_views.xml',
+        'views/estate_menus.xml',
         'views/res_users_views.xml'
     ],
-
+    'demo': [
+        "demo/demo_property_data.xml",
+    ],
 }
