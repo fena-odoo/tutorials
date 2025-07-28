a33a428726524aa53be39a869f2b6b917eb6ee83
fena-odoo <fena@odoo.com>
2025-07-28 11:52:56 +0700

    case study add signature, salesperson, total in words

---
+++ custom_invoice_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Invoice Report',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice_document_inherit.xml',
+        'views/report_tax_totals_inherit.xml',
+    ],
+    'installable': True,
+}
